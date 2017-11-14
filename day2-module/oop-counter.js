6dbb92d5d15a3e460d6178a010735341ddae29d7
Mosley <kmj3qvs@d25t20bugq17im.amer.homedepot.com>
2017-11-14 13:42:47 -0500

    Completed Counter exercise

--- day2-module/oop-counter.js
+++ day2-module/oop-counter.js
@@ -1,17 +1,26 @@
 class Counter {
   // TODO: implement the counter
+  constructor(start = 0) {
+    this.value = start;
+  }
+  increment(amt = 1) {
+    this.value += amt;
+  }
+  decrement(amt = 1) {
+    this.value = this.value > amt ? this.value - amt : 0
+  }
 }
 
 // test it out
 const counter = new Counter(10);
 console.log(counter.value);       // 10
-counter.inc();
+counter.increment();
 console.log(counter.value);       // 11
-counter.inc(100);
+counter.increment(100);
 console.log(counter.value);       // 111
-counter.dec();
+counter.decrement();
 console.log(counter.value);       // 110
-counter.dec(105);
+counter.decrement(105);
 console.log(counter.value);       // 5
-counter.dec(20);
+counter.decrement(20);
 console.log(counter.value);       // 0
