c97296eaf78cbe33fd3271692103a8dfb229a4a5
Mosley <kmj3qvs@d25t20bugq17im.amer.homedepot.com>
2017-11-14 09:03:35 -0500

    Completed array-join.js exercise

--- day1-module/array-join.js
+++ day1-module/array-join.js
@@ -1,5 +1,9 @@
 function joinArrays(a1, prop1, a2, prop2) {
   // TODO: return an array with the joined objects from a1 and a2, joined on prop1 values in a1 matching prop2 values in a2.
+  return a1.map( item1 => {
+    const match = a2.filter( item2 => item1[prop1] === item2[prop2] )[0];
+    return Object.assign({}, item1, match)
+  });
 }
 
 const directors = [
